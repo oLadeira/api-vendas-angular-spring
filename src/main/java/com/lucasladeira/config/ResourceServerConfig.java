28beba2c26984d1b2c116d4d7fdf1e48bf5ef5e5
Lucas Ladeira <lucas-ladeira@hotmail.com>
2022-01-26 12:41:19 -0300

    :sparkles: feature: configurando o Resource Server

--- src/main/java/com/lucasladeira/config/ResourceServerConfig.java
+++ src/main/java/com/lucasladeira/config/ResourceServerConfig.java
@@ -1,5 +1,25 @@
 package com.lucasladeira.config;
 
-public class ResourceServerConfig {
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
 
+@Configuration
+@EnableResourceServer
+public class ResourceServerConfig extends ResourceServerConfigurerAdapter{
+	
+	/* classe responsavel em configurar acessos aos recursos da api
+	 * no metodo configure estou mapeando qual ROLE é permitido acessar qual URL 
+	 * exemplo ROLE USER pode acessar o recurso no endereco /api/clientes/qualquercoisa*/
+	
+	@Override
+	public void configure(HttpSecurity http) throws Exception {
+		http
+		.authorizeHttpRequests()
+		//.antMatchers("/api/clientes/**").hasRole("USER") //permitindo somente USER
+		.antMatchers("/api/usuarios").permitAll() //permitindo todos
+		.antMatchers("/api/cliente/**", "/api/servicos-prestados/**").authenticated()
+		.anyRequest().denyAll();//recusar qualquer outra requisicao
+	}
 }
