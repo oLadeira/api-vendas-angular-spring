3337f8e2bef38fda4697a281b2b1bb3333e25ced
Lucas Ladeira <lucas-ladeira@hotmail.com>
2022-01-26 13:49:53 -0300

    :sparkles: feature: configurando o Authorization Server

--- src/main/java/com/lucasladeira/config/AuthorizationServerConfig.java
+++ src/main/java/com/lucasladeira/config/AuthorizationServerConfig.java
@@ -1,5 +1,13 @@
 package com.lucasladeira.config;
 
-public class AuthorizationServerConfig {
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 
+@Configuration
+@EnableAuthorizationServer
+public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdapter{
+
+	
+	
 }
